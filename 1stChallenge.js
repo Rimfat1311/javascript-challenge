6876ded9aff2b76711e4659866b3f3de7c5bb2b9
rimfat1311 <simontimnan@gmail.com>
2026-02-14 23:07:22 +0100

    update

---
+++ 1stChallenge.js
@@ -0,0 +1,31 @@
+//Trivia bot
+
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName;
+botName = "dev Tims";
+let botLocation;
+botLocation = "Mangu";
+let favoriteLanguage;
+favoriteLanguage = "Javascript"
+
+
+console.log("My name is " + botName +  " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " was created in just 10 days.";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " run on v8 engine.";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " provite functionality to web.";
+
+console.log(codingFact)
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
+
+
---
+++ script.js
@@ -0,0 +1,27 @@
+// varable
+let name = "timnan";
+let sureName = "rimfat";
+let lastName = "simon"
+let result = sureName.concat(" ", name +" ", lastName );
+console.log(result)
+
+let num1 = 5;
+let num2 = 14;
+let output = `${num1} ${num2}`;
+let sum = `${num1 + num2}`;
+console.log(output.concat(` `+ sum));
+
+
+//control flow (coditional statement)
+let salary = 12 ;
+if (salary <= 50000)
+    console.log(`your salary has been paied succesfully`)
+else{
+    console.log(`pending salary is still processing please be patient`)
+}
+
+
+
+//switch case
+
+
