862b3bb480de9b8a5aaeeb9b5c1fbd5b96163332
rimfat1311 <simontimnan@gmail.com>
2026-02-15 22:25:07 +0100

    update

---
+++ 2ndChallenge.js
@@ -0,0 +1,26 @@
+// sentence maker
+
+let adjective = "skilled";
+let noun = "chife";
+let verb = "burning";
+let place = "hotel";
+let adjective2 = "wide";
+let noun2 = "chips";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "brave";
+noun = "dragon";
+verb = "angry";
+place = "mystical forest";
+adjective2 = "flared";
+noun2 = "marshmallows";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory)
+
+
+
